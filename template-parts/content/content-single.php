d06642e71dfdf8d5b3f2ff684e18f4e09aa679e8
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-04-08 11:42:33 -0400

    swap header and img for posts

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -14,8 +14,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header alignwide">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		
 		<?php twenty_twenty_one_post_thumbnail(); ?>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
