7d6b33526dd02f584d8b89760ab836611341773d
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-04-08 11:49:21 -0400

    swap that font size again

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -14,7 +14,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header alignwide">
-		
 		<?php twenty_twenty_one_post_thumbnail(); ?>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
