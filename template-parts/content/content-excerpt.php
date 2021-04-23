6033012a230c3af3dc7467cb3c54b1b415bfe1b5
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-04-23 14:50:05 -0400

    well that wasn't important...

--- template-parts/content/content-excerpt.php
+++ template-parts/content/content-excerpt.php
@@ -18,7 +18,7 @@
 	<div class="entry-content">
 		<h5 class="entry-date gray"><?php echo get_the_date(); ?></h5>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<h5 class="entry-author">By <?php the_author(); ?></h5>
+		<h5 class="entry-author"><?php the_author(); ?></h5>
 		<?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?>
 	</div><!-- .entry-content -->
 </article><!-- #post-${ID} -->
