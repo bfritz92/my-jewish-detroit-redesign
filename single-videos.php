118057ea90358c28d9118975853f00a85d04b6ab
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-04-23 11:43:42 -0400

    all of Herb's changes

--- single-videos.php
+++ single-videos.php
@@ -15,7 +15,45 @@ get_header();
 while ( have_posts() ) :
 	the_post();
 
-	get_template_part( 'template-parts/content/content-single' );
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header default-max-width">
+		<?php twenty_twenty_one_post_thumbnail(); ?>
+		
+		<h5 class="entry-date gray"><?php echo get_the_date(); ?></h5>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<h4 class="entry-excerpt gray"><?php the_excerpt(); ?></h4>
+		<h5 class="entry-author"><?php the_author(); ?></h5>
+		
+		
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+		the_content();
+
+		wp_link_pages(
+			array(
+				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
+				'after'    => '</nav>',
+				/* translators: %: Page number. */
+				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer default-max-width">
+		<?php twenty_twenty_one_entry_meta_footer(); ?>
+	</footer><!-- .entry-footer -->
+
+	<?php if ( ! is_singular( 'attachment' ) ) : ?>
+		<?php get_template_part( 'template-parts/post/author-bio' ); ?>
+	<?php endif; ?>
+
+</article><!-- #post-<?php the_ID(); ?> -->
+<?php	
 
 	if ( is_attachment() ) {
 		// Parent post navigation.
