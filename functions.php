d68ffe7b36dac5beb05b30eb1221af72b59c8e3f
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-04-23 11:56:05 -0400

    slap some code in the functions file....

--- functions.php
+++ functions.php
@@ -627,3 +627,19 @@ function twentytwentyone_add_ie_class() {
 	<?php
 }
 add_action( 'wp_footer', 'twentytwentyone_add_ie_class' );
+
+//remove the category: part of caetgory and tag page titles
+add_filter( 'get_the_archive_title', function ($title) {    
+	if ( is_category() ) {    
+			$title = single_cat_title( '', false );    
+		} elseif ( is_tag() ) {    
+			$title = single_tag_title( '', false );    
+		} elseif ( is_author() ) {    
+			$title = '<span class="vcard">' . get_the_author() . '</span>' ;    
+		} elseif ( is_tax() ) { //for custom post types
+			$title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+		} elseif (is_post_type_archive()) {
+			$title = post_type_archive_title( '', false );
+		}
+	return $title;    
+});
