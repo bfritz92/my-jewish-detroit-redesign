f399605959806b1d632d53f23cff4280444748d6
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-04-23 14:44:59 -0400

    maybe that'll fix it...

--- template-parts/content/content-excerpt.php
+++ template-parts/content/content-excerpt.php
@@ -14,8 +14,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<?php get_template_part( 'template-parts/header/excerpt-header', get_post_format() ); ?>
-	
+
 	<div class="entry-content">
+		<h5 class="entry-date gray"><?php echo get_the_date(); ?></h5>
 		<?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?>
 	</div><!-- .entry-content -->
 </article><!-- #post-${ID} -->
--- template-parts/excerpt/excerpt.php
+++ template-parts/excerpt/excerpt.php
@@ -8,9 +8,5 @@
  * @subpackage Twenty_Twenty_One
  * @since Twenty Twenty-One 1.0
  */
-
-
-<h5 class="entry-date gray"><?php echo get_the_date(); ?></h5>
-		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-		<h4 class="entry-excerpt gray"><?php the_excerpt(); ?></h4>
-		<h5 class="entry-author"><?php the_author(); ?></h5>
+the_author();
+the_excerpt();
