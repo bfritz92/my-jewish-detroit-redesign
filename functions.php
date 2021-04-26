c0fcf5f744f16c697f65fd21957cd18aeba8cd8a
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-04-26 14:19:20 -0400

    limit the excerpt length?

--- functions.php
+++ functions.php
@@ -643,3 +643,16 @@ add_filter( 'get_the_archive_title', function ($title) {
 		}
 	return $title;    
 });
+
+//limit excerpt length
+function excerpt($limit) {
+	$excerpt = explode(' ', get_the_excerpt(), $limit);
+	if (count($excerpt)>=$limit) {
+	  array_pop($excerpt);
+	  $excerpt = implode(" ",$excerpt).'...';
+	} else {
+	  $excerpt = implode(" ",$excerpt);
+	}	
+	$excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+	return $excerpt;
+  }
