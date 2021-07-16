1046051dc06a3f40af80017026feb8032990b325
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-07-16 14:00:57 -0400

    add herb's related posts

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -41,6 +41,39 @@
 
 	<footer class="entry-footer default-max-width">
 		<?php twenty_twenty_one_entry_meta_footer(); ?>
+	<section class="related-articles" style="background-color:#FFFF00;">
+	    <?php
+            $orig_post = $post;
+            global $post;
+            $tags = wp_get_post_tags($post->ID);
+           
+            if ($tags) {
+                $tag_ids = array();
+                foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+                $args=array(
+                    'tag__in' => $tag_ids,
+                    'post__not_in' => array($post->ID),
+                    'posts_per_page'=>3, // Number of related posts to display.
+                    'caller_get_posts'=>1
+                );
+           
+            $my_query = new wp_query( $args );
+         
+            while( $my_query->have_posts() ) {
+                $my_query->the_post();
+        ?>
+        <div class="related-articles--item">
+            <a href="<?php the_permalink()?>"><img class="related-articles--item--img"></a>
+            <a href="<?php the_permalink()?>"><h3 class="related-articles--item--title"><?php the_title(); ?></h3></a>
+            <p class="related-articles--item--excerpt"><?php the_excerpt(); ?></p>
+            <a href="<?php the_permalink()?>" class="related-articles--item--link">Link</a>
+        </div>
+        <?php }
+          }
+            $post = $orig_post;
+            wp_reset_query();
+        ?>
+    </section>
 	</footer><!-- .entry-footer -->
 
 	<?php if ( ! is_singular( 'attachment' ) ) : ?>
