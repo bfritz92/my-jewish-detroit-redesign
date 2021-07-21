0b02da05a10c71775f428d2b7a0ab7feb6523e49
brian <fritz@jfmd.org>
2021-07-21 16:15:55 -0400

    teplate tags. might screw it all up

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -123,22 +123,22 @@ if ( ! function_exists( 'twenty_twenty_one_entry_meta_footer' ) ) {
 			}
 		} else {
 
-			echo '<div class="posted-by">';
+			//echo '<div class="posted-by">';
 			// Posted on.
-			twenty_twenty_one_posted_on();
+			//twenty_twenty_one_posted_on();
 			// Posted by.
-			twenty_twenty_one_posted_by();
+			//twenty_twenty_one_posted_by();
 			// Edit post link.
-			edit_post_link(
-				sprintf(
+			//edit_post_link(
+				//sprintf(
 					/* translators: %s: Name of current post. Only visible to screen readers. */
-					esc_html__( 'Edit %s', 'twentytwentyone' ),
-					'<span class="screen-reader-text">' . get_the_title() . '</span>'
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			echo '</div>';
+					//esc_html__( 'Edit %s', 'twentytwentyone' ),
+					//'<span class="screen-reader-text">' . get_the_title() . '</span>'
+				//),
+				//'<span class="edit-link">',
+				//'</span>'
+			//);
+			//echo '</div>';
 
 			if ( has_category() || has_tag() ) {
 
