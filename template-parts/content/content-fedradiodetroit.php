54d1f54167f6f6292eefc171f18317ef5b05d7e0
brian <fritz@jfmd.org>
2021-07-22 14:48:13 -0400

    add fedradio template

---
+++ template-parts/content/content-fedradiodetroit.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Template part for displaying FedRadio Detroit posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header default-max-width">
+		<?php // - Default Thumbnail call twenty_twenty_one_post_thumbnail(); ?>
+		<?php the_post_thumbnail( 'full' ); ?>
+        <h1>IT'S WORKING!</h1>
+		<h5 class="entry-date gray"><?php echo get_the_date(); ?></h5>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        <h5 class="entry-author"><?php the_author(); ?></h5>
+		<h4 class="entry-excerpt gray"><?php the_excerpt(); ?></h4>
+		
+		
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+		the_content();
+
+		wp_link_pages(
+			array(
+				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
+				'after'    => '</nav>',
+				/* translators: %: Page number. */
+				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer default-max-width">
+		<?php twenty_twenty_one_entry_meta_footer(); ?>
+	</footer><!-- .entry-footer -->
+    <div class="alignwide related-articles--heading">
+        <h2>You may also like...</h2>
+    </div>
+	<section class="related-articles alignwide">
+	    <?php
+            $orig_post = $post;
+            global $post;
+            $tags = wp_get_post_tags($post->ID);
+           
+            if ($tags) {
+                $tag_ids = array();
+                foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+                $args=array(
+                    'tag__in' => $tag_ids,
+                    'post__not_in' => array($post->ID),
+                    'posts_per_page'=>3, // Number of related posts to display.
+                    'caller_get_posts'=>1
+                );
+           
+            $my_query = new wp_query( $args );
+         
+            while( $my_query->have_posts() ) {
+                $my_query->the_post();
+        ?>
+        <div class="related-articles--item">
+            
+            <a href="<?php the_permalink()?>" class="related-articles--item--img"><?php the_post_thumbnail(); ?></a>
+            <div class="related-articles--item--content">
+                <a href="<?php the_permalink()?>"><h3 class="related-articles--item--title"><?php the_title(); ?></h3></a>
+                <p class="related-articles--item--excerpt"><?php the_excerpt(); ?></p>
+                <a href="<?php the_permalink()?>" class="related-articles--item--link">Read More</a>
+            </div>
+        </div>
+        <?php }
+          }
+            $post = $orig_post;
+            wp_reset_query();
+        ?>
+    </section>
+
+</article><!-- #post-<?php the_ID(); ?> -->
