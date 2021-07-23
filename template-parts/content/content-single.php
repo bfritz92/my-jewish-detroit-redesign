1b3823f1a0c62a02353539aca176c8b5254adf36
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-07-23 15:41:41 -0400

    get the category....list?

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -17,7 +17,7 @@
 		<?php // - Default Thumbnail call twenty_twenty_one_post_thumbnail(); ?>
         
 		<?php the_post_thumbnail( 'full' ); ?>
-        <p class="entry-category"><?php echo get_the_category(); ?></p>
+        <p class="entry-category"><?php echo get_the_category_list(); ?></p>
         <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<h4 class="entry-excerpt"><?php the_excerpt(); ?></h4>
         <p class="entry-date"><?php echo get_the_date(); ?></p>
