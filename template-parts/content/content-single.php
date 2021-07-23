a72d49949174ae2f3a7c5c6ae9caddc4ab5317d9
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-07-23 15:18:57 -0400

    re-order the headers

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -16,10 +16,11 @@
 	<header class="entry-header default-max-width">
 		<?php // - Default Thumbnail call twenty_twenty_one_post_thumbnail(); ?>
 		<?php the_post_thumbnail( 'full' ); ?>
-		<h5 class="entry-date gray"><?php echo get_the_date(); ?></h5>
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-        <h5 class="entry-author"><?php the_author(); ?></h5>
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<h4 class="entry-excerpt gray"><?php the_excerpt(); ?></h4>
+        <p class="entry-date gray"><?php echo get_the_date(); ?></p>
+        <h5 class="entry-author">By <?php the_author(); ?></h5>
+		
 		
 		
 	</header><!-- .entry-header -->
