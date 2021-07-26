d45034382fc20f44e1c459df10dbb1b464108693
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-07-26 12:02:31 -0400

    reconcile with single.php

--- single-fedradiodetroit.php
+++ single-fedradiodetroit.php
@@ -22,10 +22,11 @@ while ( have_posts() ) :
 	<header class="fedradio entry-header default-max-width">
 		<?php // - Default Thumbnail call twenty_twenty_one_post_thumbnail(); ?>
 		<?php the_post_thumbnail( 'full' ); ?>
-		<h5 class="entry-date gray"><?php echo get_the_date(); ?></h5>
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<h4 class="entry-excerpt gray"><?php the_excerpt(); ?></h4>
-		<h5 class="entry-author"><?php the_author(); ?></h5>
+        <p class="entry-category"><?php echo get_the_category_list(); ?></p>
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<h4 class="entry-excerpt"><?php the_excerpt(); ?></h4>
+        <p class="entry-date"><?php echo get_the_date(); ?></p>
+        <h5 class="entry-author">By <?php the_author(); ?></h5>
 		<a class="spreaker-player" href="http://www.spreaker.com/episode/<?php the_field ('podcast_embed'); ?>" data-resource="episode_id=<?php the_field ('podcast_embed'); ?>" data-width="100%" data-height="200px" data-theme="light" data-playlist="false" data-playlist-continuous="false" data-autoplay="false" data-live-autoplay="false" data-chapters-image="true" data-episode-image-position="right" data-hide-logo="false" data-hide-likes="false" data-hide-comments="false" data-hide-sharing="false" data-hide-download="true">Listen to "<?php the_title(); ?>"</a>
 	
 		
