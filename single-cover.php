2b83f7edd9b91683b29ef9f81c6bc8981ddd52f9
brian <fritz@jfmd.org>
2021-08-05 15:50:04 -0400

    finer adjustments

--- single-cover.php
+++ single-cover.php
@@ -22,7 +22,7 @@ while ( have_posts() ) :
 	<header class="cover entry-header alignfull wp-block-cover alignfull">	
 			<?php the_post_thumbnail( 'large', ['class' => 'wp-block-cover__image-background'] ); ?>
 			<div class="wp-block-cover__inner-container">
-				<p class="entry-category"><?php echo get_the_category_list(); ?></p>
+				<?php echo get_the_category_list(); ?>
 				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 				<h4 class="entry-excerpt"><?php the_excerpt(); ?></h4>
 				<h5 class="entry-author">By <?php the_author(); ?></h5>
