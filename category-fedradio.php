18913ec835d709b8a261f80dfcc44d112b3d8082
Herb Hanson <hanson@local.jfmd.org>
2021-08-10 14:25:56 -0400

    fed

--- category-fedradio.php
+++ category-fedradio.php
@@ -26,6 +26,7 @@ $description = get_the_archive_description();
 	<?php while ( have_posts() ) : ?>
 		<?php the_post(); ?>
 		<?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
+fffff
 	<?php endwhile; ?>
 
 	<?php twenty_twenty_one_the_posts_navigation(); ?>
