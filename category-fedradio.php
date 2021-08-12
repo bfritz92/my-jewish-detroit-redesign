c2f19819c6a9348893ce8b56d586e2ec747c783e
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-08-12 13:33:15 -0400

    add description/img/links to fedradio

--- category-fedradio.php
+++ category-fedradio.php
@@ -19,7 +19,15 @@ $description = get_the_archive_description();
 	<header class="page-header alignwide">
 		<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
 		<?php if ( $description ) : ?>
-			<div class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></div>
+			<div class="archive-description">
+				<?php echo wp_kses_post( wpautop( $description ) ); ?>
+				<figure class="archive-description--img"><img src="https://mjdasset.s3.us-west-2.amazonaws.com/wp-content/uploads/2021/08/12172448/FedRadioDetroit-2c-sm.jpg"></figure>
+			</div>
+			<ul class="inline-links">
+				<li><a href="https://podcasts.apple.com/gb/podcast/fedradio-detroit/id1494716703">Apple Podcasts</a></li>
+				<li><a href="https://podcasts.google.com/feed/aHR0cHM6Ly93d3cuc3ByZWFrZXIuY29tL3Nob3cvNDE1OTA0MC9lcGlzb2Rlcy9mZWVk">Google Podcasts</a></li>
+				<li><a href="https://open.spotify.com/show/7GDJspmdbNmtkcqmxxgHqK">Spotify</a></li>
+			</ul>
 		<?php endif; ?>
 	</header><!-- .page-header -->
 
@@ -28,7 +36,7 @@ $description = get_the_archive_description();
 		<?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
 		<?php if($postCount == 1) : ?>		
 			<article>
-				<a class="spreaker-player" href="http://www.spreaker.com/episode/<?php the_field ('podcast_embed'); ?>" data-resource="episode_id=<?php the_field ('podcast_embed'); ?>" data-width="100%" data-height="200px" data-theme="light" data-playlist="false" data-playlist-continuous="false" data-autoplay="false" data-live-autoplay="false" data-chapters-image="true" data-episode-image-position="right" data-hide-logo="false" data-hide-likes="false" data-hide-comments="false" data-hide-sharing="false" data-hide-download="true">Listen to "<?php the_title(); ?>"</a>
+				<a class="spreaker-player" href="http://www.spreaker.com/episode/<?php the_field ('podcast_embed'); ?>" data-resource="episode_id=<?php the_field ('podcast_embed'); ?>" data-width="100%" data-height="200px" data-theme="light" data-playlist="true" data-playlist-continuous="true" data-autoplay="false" data-live-autoplay="false" data-chapters-image="true" data-episode-image-position="right" data-hide-logo="true" data-hide-likes="true" data-hide-comments="true" data-hide-sharing="false" data-hide-download="true">Listen to "<?php the_title(); ?>"</a>
 			</article>
 		<?php endif; ?>
 	<?php endwhile; ?>
