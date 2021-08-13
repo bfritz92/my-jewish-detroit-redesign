982adfed945242d8441addf003647234158a5252
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-08-13 15:19:53 -0400

    make them outline style

--- category-fedradio.php
+++ category-fedradio.php
@@ -21,10 +21,10 @@ $description = get_the_archive_description();
 		<?php if ( $description ) : ?>
 			<div class="archive-description">
 				<?php echo wp_kses_post( wpautop( $description ) ); ?>
-				<ul class="inline-links wp-block-buttons is-style-outline">
-					<li class="wp-block-button"><a href="https://podcasts.apple.com/gb/podcast/fedradio-detroit/id1494716703" class="wp-block-button__link">Apple Podcasts</a></li>
-					<li class="wp-block-button"><a href="https://podcasts.google.com/feed/aHR0cHM6Ly93d3cuc3ByZWFrZXIuY29tL3Nob3cvNDE1OTA0MC9lcGlzb2Rlcy9mZWVk" class="wp-block-button__link">Google Podcasts</a></li>
-					<li class="wp-block-button"><a href="https://open.spotify.com/show/7GDJspmdbNmtkcqmxxgHqK" class="wp-block-button__link">Spotify</a></li>
+				<ul class="inline-links wp-block-buttons ">
+					<li class="wp-block-button is-style-outline"><a href="https://podcasts.apple.com/gb/podcast/fedradio-detroit/id1494716703" class="wp-block-button__link">Apple Podcasts</a></li>
+					<li class="wp-block-button is-style-outline"><a href="https://podcasts.google.com/feed/aHR0cHM6Ly93d3cuc3ByZWFrZXIuY29tL3Nob3cvNDE1OTA0MC9lcGlzb2Rlcy9mZWVk" class="wp-block-button__link">Google Podcasts</a></li>
+					<li class="wp-block-button is-style-outline"><a href="https://open.spotify.com/show/7GDJspmdbNmtkcqmxxgHqK" class="wp-block-button__link">Spotify</a></li>
 				</ul>
 				<figure class="archive-description--img"><img src="https://mjdasset.s3.us-west-2.amazonaws.com/wp-content/uploads/2021/08/12172448/FedRadioDetroit-2c-sm.jpg"></figure>
 			</div>
