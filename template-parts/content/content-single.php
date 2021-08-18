474eab51cc12b97a12d4325dee22dc73c07fe2ca
brian <fritz@jfmd.org>
2021-08-18 16:01:39 -0400

    make those featured pic WIDE

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -13,7 +13,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header default-max-width">
+	<header class="entry-header alignwide">
 		<?php // - Default Thumbnail call twenty_twenty_one_post_thumbnail(); ?>
         
 		<?php the_post_thumbnail( 'full' ); ?>
