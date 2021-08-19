e02895a90fe9458217d6be4220d83c1c96f2167e
brian <fritz@jfmd.org>
2021-08-19 14:02:59 -0400

    put the post info in a box...

--- footer.php
+++ footer.php
@@ -19,7 +19,21 @@
 	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-
+	<div class="footer--top">
+		<div class="site-name">
+				<?php if ( has_custom_logo() ) : ?>
+					<div class="site-logo"><?php the_custom_logo(); ?></div>
+				<?php else : ?>
+					<?php if ( get_bloginfo( 'name' ) && get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
+						<?php if ( is_front_page() && ! is_paged() ) : ?>
+							<?php bloginfo( 'name' ); ?>
+						<?php else : ?>
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+						<?php endif; ?>
+					<?php endif; ?>
+				<?php endif; ?>
+			</div><!-- .site-name -->
+	</div>
 		<?php if ( has_nav_menu( 'footer' ) ) : ?>
 			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
 				<ul class="footer-navigation-wrapper">
@@ -40,19 +54,7 @@
 			</nav><!-- .footer-navigation -->
 		<?php endif; ?>
 		<div class="site-info">
-			<div class="site-name">
-				<?php if ( has_custom_logo() ) : ?>
-					<div class="site-logo"><?php the_custom_logo(); ?></div>
-				<?php else : ?>
-					<?php if ( get_bloginfo( 'name' ) && get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
-						<?php if ( is_front_page() && ! is_paged() ) : ?>
-							<?php bloginfo( 'name' ); ?>
-						<?php else : ?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						<?php endif; ?>
-					<?php endif; ?>
-				<?php endif; ?>
-			</div><!-- .site-name -->
+			
 			
 				<a class="powered-by-jfmd" href="https://www.jewishdetroit.org">
 					<img src="https://mjdasset.s3.us-west-2.amazonaws.com/wp-content/uploads/2021/08/18130508/Powered-by-JFMD.svg">
--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -17,11 +17,13 @@
 		<?php // - Default Thumbnail call twenty_twenty_one_post_thumbnail(); ?>
         
 		<?php the_post_thumbnail( 'full' ); ?>
+        <div class="post-info align-default-width">
         <p class="entry-category"><?php echo get_the_category_list(); ?></p>
         <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<h4 class="entry-excerpt"><?php the_excerpt(); ?></h4>
         <h5 class="entry-author">By <?php the_author(); ?></h5>
         <h5 class="entry-date"><?php echo get_the_date(); ?></h5>
+        </div>
 		
 		
 		
