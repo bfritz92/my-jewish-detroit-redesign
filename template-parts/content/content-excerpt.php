9cb4397ac1b0431873ae32de31a5703dca37f9a3
brian <fritz@jfmd.org>
2021-08-25 09:04:25 -0400

    geez, too many <p> rules

--- template-parts/content/content-excerpt.php
+++ template-parts/content/content-excerpt.php
@@ -19,7 +19,7 @@
 		<h5 class="entry-date gray"><?php echo get_the_date(); ?></h5>
 		<a href="<?php the_permalink(); ?>"> <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?> </a>
 		<h5 class="entry-author gray alignleft"><?php the_author(); ?></h5>
-		<p class="alignwide entry-excerpt"><?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?></p>
+		<h5 class="alignwide entry-excerpt"><?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?></h5>
 		<!-- MJD - Herb: The excerpt already outputs "Continue Reading if over 30 words" <a href="<?php the_permalink(); ?>">Read More</a> -->
 	</div><!-- .entry-content -->
 </article><!-- #post-${ID} -->
