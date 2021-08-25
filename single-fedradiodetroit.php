4e792ea6049dfb8a2826e21b32ccd3bc405611f5
brian <fritz@jfmd.org>
2021-08-25 14:30:43 -0400

    reconcile standard and FedRadio pages

--- single-fedradiodetroit.php
+++ single-fedradiodetroit.php
@@ -19,18 +19,28 @@ while ( have_posts() ) :
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="fedradio entry-header default-max-width post-info">
+<header class="entry-header alignwide">
 		<?php // - Default Thumbnail call twenty_twenty_one_post_thumbnail(); ?>
+        
 		<?php the_post_thumbnail( 'full' ); ?>
-        <p class="entry-category"><?php echo get_the_category_list(); ?></p>
-        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<h4 class="entry-excerpt"><?php the_excerpt(); ?></h4>
-		<h5 class="entry-author">By <?php the_author(); ?></h5>
-        <h5 class="entry-date"><?php echo get_the_date(); ?></h5>
-		<a class="spreaker-player" href="http://www.spreaker.com/episode/<?php the_field ('podcast_embed'); ?>" data-resource="episode_id=<?php the_field ('podcast_embed'); ?>" data-width="100%" data-height="200px" data-theme="light" data-playlist="false" data-playlist-continuous="false" data-autoplay="false" data-live-autoplay="false" data-chapters-image="true" data-episode-image-position="right" data-hide-logo="false" data-hide-likes="false" data-hide-comments="false" data-hide-sharing="false" data-hide-download="true">Listen to "<?php the_title(); ?>"</a>
+       
+		
+		
+		
 	</header><!-- .entry-header -->
 
+	<div class="entry-content">
+        <div class="post-info">
+            <p class="entry-category"><?php echo get_the_category_list(); ?></p>
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <h4 class="entry-excerpt"><?php the_excerpt(); ?></h4>
+            <h5 class="entry-author">By <?php the_author(); ?></h5>
+            <h5 class="entry-date"><?php echo get_the_date(); ?></h5>
+        </div>
+        <hr /><!-- .entry-header -->
+
 	<div class="fedradio entry-content">
+		<a class="spreaker-player" href="http://www.spreaker.com/episode/<?php the_field ('podcast_embed'); ?>" data-resource="episode_id=<?php the_field ('podcast_embed'); ?>" data-width="100%" data-height="200px" data-theme="light" data-playlist="false" data-playlist-continuous="false" data-autoplay="false" data-live-autoplay="false" data-chapters-image="true" data-episode-image-position="right" data-hide-logo="false" data-hide-likes="false" data-hide-comments="false" data-hide-sharing="false" data-hide-download="true">Listen to "<?php the_title(); ?>"</a>
 		<?php
 		the_content();
 
