68bfcb5de8bdf37be4eeb6c5af29b7cbdad92567
brian <fritz@jfmd.org>
2021-08-31 09:52:11 -0400

    add temp splash

---
+++ temporary-splash.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name Posts: Temporary Splash Page
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header(); 
+?>
+
+<header class="page-header alignwide temp-splash">
+		<h1 class="page-title">Coming Soon</h1>
+	</header><!-- .page-header -->
+
+	<div class="default-max-width">
+		<div class="page-content">
+			<p class="align-center">We're under construction right now, but plan to come back soon with a whole new look!</p>
+		</div><!-- .page-content -->
+	</div>
+
+
+<?php get_footer();
