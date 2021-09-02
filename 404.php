a7848b9f890506f39fa593952ca2953c7b94a093
brian <fritz@jfmd.org>
2021-09-02 12:43:43 -0400

    alignwide 404

--- 404.php
+++ 404.php
@@ -16,7 +16,7 @@ get_header();
 		<h1 class="page-title"><?php esc_html_e( 'Nothing here', 'twentytwentyone' ); ?></h1>
 	</header><!-- .page-header -->
 
-	<div class="error-404 not-found default-max-width">
+	<div class="error-404 not-found alignwide">
 		<div class="page-content">
 			<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentytwentyone' ); ?></p>
 			<?php get_search_form(); ?>
